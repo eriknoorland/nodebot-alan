3abbcf3d9c7076ef54a4243d8b7cfa88f550f951
Erik Noorland <mail@eriknoorland.nl>
2021-10-01 10:50:43 +0200

    Update robots physical dimensions, Extend distance test data logs

--- src/programs/testDistance.js
+++ src/programs/testDistance.js
@@ -8,12 +8,43 @@ module.exports = (distance) => ({ config, arena, logger, controllers, sensors })
   async function start() {
     logger.log('start', 'testDistance');
 
+    let totalLeftTicks = 0;
+    let totalRightTicks = 0;
+
+    motion.on('odometry', ({ leftTicks, rightTicks }) => {
+      totalLeftTicks += leftTicks;
+      totalRightTicks += rightTicks;
+    });
+
     motion.setTrackPose(true);
     motion.appendPose({ x: 200, y: arena.height * 0.75, phi: 0 });
 
-    await motion.distanceHeading(distance, 0);
+    console.log(
+      config.LEFT_WHEEL_DIAMETER,
+      config.LEFT_WHEEL_CIRCUMFERENCE,
+      config.LEFT_DISTANCE_PER_TICK,
+      config.RIGHT_WHEEL_DIAMETER,
+      config.RIGHT_WHEEL_CIRCUMFERENCE,
+      config.RIGHT_DISTANCE_PER_TICK,
+    );
+
+    await motion.distanceCalibrationTest(distance);
+
+    console.log({
+      totalLeftTicks,
+      totalRightTicks,
+    });
+
+    console.log('virtual distance left:', totalLeftTicks * config.LEFT_DISTANCE_PER_TICK);
+    console.log('virtual distance right:', totalRightTicks * config.RIGHT_DISTANCE_PER_TICK);
 
     testComplete();
+  }
+
+  function loop() {
+    return new Promise(resolve => {
+      const interval = setInterval();
+    });
   }
 
   function stop() {
