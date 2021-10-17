995e4f908abbc0db2b2aee466209101503c1026d
Erik Noorland <mail@eriknoorland.nl>
2021-10-17 09:37:01 +0200

    Clean up verify position and rotation helpers

--- src/helpers/verifyPosition.js
+++ src/helpers/verifyPosition.js
@@ -1,14 +1,17 @@
 const scan = require('../utils/sensor/lidar/scan');
 const averageMeasurements = require('../utils/sensor/lidar/averageMeasurements');
 
-const verifyPosition = async (arena, lidar) => {
-  const measurements = await scan(lidar, 2000);
+const verifyPosition = async (arena, lidar, motion, heading) => {
+  const measurements = await scan(lidar, 1000);
   const averagedMeasurements = averageMeasurements(measurements);
 
-  return Promise.resolve({
-    x: averagedMeasurements[180],
+  motion.appendPose({
+    x: averagedMeasurements[180] || averagedMeasurements[179] || averagedMeasurements[181],
     y: arena.height - averagedMeasurements[90],
+    phi: heading,
   });
+
+  return Promise.resolve();
 };
 
 module.exports = verifyPosition;
--- src/helpers/verifyRotation.js
+++ src/helpers/verifyRotation.js
@@ -3,7 +3,7 @@ const scan = require('../utils/sensor/lidar/scan');
 const averageMeasurements = require('../utils/sensor/lidar/averageMeasurements');
 const filterMeasurements = require('../utils/sensor/lidar/filterMeasurements');
 
-const { rad2deg, deg2rad } = robotlib.utils.math;
+const { deg2rad } = robotlib.utils.math;
 
 const verifyRotation = async (lidar, motion, angle, openingAngle) => {
   const minAngle = angle - (openingAngle / 2);
@@ -24,7 +24,6 @@ const verifyRotation = async (lidar, motion, angle, openingAngle) => {
       points.push({ x, y });
     });
 
-
   const firstPoint = points[0];
   const lastPoint = points[points.length - 1];
   const s = firstPoint.x - lastPoint.x;
@@ -32,35 +31,6 @@ const verifyRotation = async (lidar, motion, angle, openingAngle) => {
   const sin = Math.sin(o / s);
 
   await motion.rotate(sin);
-
-  // const measurements = await scan(lidar, 2000);
-  // const averagedMeasurements = averageMeasurements(measurements);
-  // const angleOffset = openingAngle / 2;
-  // const centerAngleDistance = averagedMeasurements[angle];
-  // const minAngleDistance = averagedMeasurements[minAngle];
-  // const maxAngleDistance = averagedMeasurements[maxAngle];
-
-  // const a = centerAngleDistance;
-  // const o = Math.tan(deg2rad(angleOffset)) * a;
-  // const s = Math.sqrt(Math.pow(a, 2) + Math.pow(o, 2));
-  // const A = maxAngleDistance - s;
-  // const b = Math.tan(A / o) * -1;
-
-  // console.log(
-  //   { minAngleDistance, maxAngleDistance, a, o, s, A, b },
-  //   rad2deg(b),
-  // );
-
-  // await motion.rotate(b);
-
-  const testMeasurements = await scan(lidar, 2000);
-  const testAveragedMeasurements = averageMeasurements(testMeasurements);
-
-  console.log(
-    testAveragedMeasurements[minAngle],
-    testAveragedMeasurements[angle],
-    testAveragedMeasurements[maxAngle],
-  );
 };
 
 module.exports = verifyRotation;
