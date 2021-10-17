369afd0409839582b036873d6a1ac7f9bc4d0182
Erik Noorland <mail@eriknoorland.nl>
2021-10-17 09:57:28 +0200

    Update can store positions, Verify position and rotation after returning each can, Add extra scan position in C area

--- src/programs/cans.js
+++ src/programs/cans.js
@@ -4,6 +4,8 @@ const averageMeasurements = require('../utils/sensor/lidar/averageMeasurements')
 const solveStartVector = require('../utils/motion/solveStartVector2');
 const gotoStartPosition = require('../utils/motion/gotoStartPosition');
 const getInitialPosition = require('../utils/motion/getInitialPosition');
+const verifyPosition = require('../helpers/verifyPosition');
+const verifyRotation = require('../helpers/verifyRotation');
 const getArenaMatrix = require('../utils/getArenaMatrix');
 const localiseCans = require('../utils/localiseCans');
 const locateCan = require('../utils/locateCan');
@@ -22,14 +24,17 @@ module.exports = (pickupAndReturn = false) => ({ socket, config, arena, logger, 
   const maxNumCans = 6;
   const canStoreCoordinates = [
     { x: 100, y: 150 + halfArenaHeight },
-    { x: 200, y: 150 + halfArenaHeight },
-    { x: 300, y: 150 + halfArenaHeight },
-    { x: 100, y: 250 + halfArenaHeight },
-    { x: 200, y: 250 + halfArenaHeight },
-    { x: 300, y: 250 + halfArenaHeight },
+    { x: 100, y: 300 + halfArenaHeight },
+    { x: 100, y: 450 + halfArenaHeight },
+    { x: 250, y: 150 + halfArenaHeight },
+    { x: 250, y: 300 + halfArenaHeight },
+    { x: 250, y: 450 + halfArenaHeight },
   ];
 
-  const endPosition = { x: 200, y: arena.height * 0.75 };
+  const endPosition = {
+    x: 200,
+    y: arena.height * 0.75,
+  };
 
   const arenaCenterPosition = {
     x: arena.width / 2,
@@ -45,12 +50,29 @@ module.exports = (pickupAndReturn = false) => ({ socket, config, arena, logger, 
   async function start() {
     logger.log('start', 'cans');
 
-    const initialPosition = await verifyPosition();
+    await solveStartVector(lidar, motion);
+
+    const positionScanData = await scan(lidar, 1000);
+    const positionAveragedMeasurements = averageMeasurements(positionScanData);
+    await gotoStartPosition(positionAveragedMeasurements, motion);
+
+    const currentPositionScanData = await scan(lidar, 1000);
+    const currentPositionAveragedMeasurements = averageMeasurements(currentPositionScanData);
+    const initialPosition = getInitialPosition(currentPositionAveragedMeasurements, arena.height);
+
+    motion.setTrackPose(true);
+    motion.appendPose({ ...initialPosition, phi: 0 });
+
     const startPosition = { ...initialPosition };
 
     if (startPosition.x < 450) {
       startPosition.x = 450;
     }
+
+    const verificationPosition = {
+      ...startPosition,
+      y: startPosition.y + 150,
+    };
 
     const scanRadius = arena.width / 4;
     const scanPositions = [
@@ -58,23 +80,27 @@ module.exports = (pickupAndReturn = false) => ({ socket, config, arena, logger, 
       { x: 1250, y: initialPosition.y, heading: 0 },
       { x: 2050, y: initialPosition.y, heading: 0 },
       { x: 2850, y: initialPosition.y, heading: 0 },
-      // { x: 1800, y: 600, heading: -(Math.PI / 2) },
+      { x: 1800, y: 1100, heading: -(Math.PI / 2) },
+      { x: 1800, y: 600, heading: -(Math.PI / 2) },
     ];
 
     for (let scanPositionIndex = 0; scanPositionIndex < scanPositions.length; scanPositionIndex += 1) {
       const scanPosition = scanPositions[scanPositionIndex];
       const isAtLastScanPosition = scanPositionIndex === scanPositions.length - 1;
-      const inSquareC = scanPosition.y < halfArenaHeight;
+      const isScanPositionInSquareC = isPositionInAreaC(halfArenaHeight, scanPosition);
 
-      if (scanPositionIndex > 0) {
-        if (inSquareC) {
-          await motion.move2XY(arenaCenterPosition);
-          await pause(250);
-        }
-
-        await motion.move2XYPhi(scanPosition, scanPosition.heading);
+      if (isScanPositionInSquareC) {
+        await motion.move2XY(arenaCenterPosition);
         await pause(250);
       }
+
+      await motion.move2XYPhi(scanPosition, scanPosition.heading);
+      await pause(250);
+
+      // if (scanPosition.heading === 0) {
+      //   await verifyRotation(lidar, motion, 90, 60);
+      //   await verifyPosition(arena, lidar, motion, 0);
+      // }
 
       const scanPose = motion.getPose();
       const localisedCans = await localiseCans(scanRadius, matrix, scanPose, lidar);
@@ -86,13 +112,12 @@ module.exports = (pickupAndReturn = false) => ({ socket, config, arena, logger, 
 
       // visualize the matrix
       matrix.forEach(row => console.log(row.toString()));
-
       console.log(`${localisedCans.length} can(s) found at scan position ${scanPosition.x},${scanPosition.y}`);
 
       for (let obstacleIndex = 0; obstacleIndex < sortedLocalisedCans.length; obstacleIndex += 1) {
         const obstacle = sortedLocalisedCans[obstacleIndex];
 
-        if (inSquareC) {
+        if (obstacleIndex !== 0 && isPositionInAreaC(halfArenaHeight, obstacle)) {
           await motion.move2XY(arenaCenterPosition);
           await pause(250);
         }
@@ -112,7 +137,7 @@ module.exports = (pickupAndReturn = false) => ({ socket, config, arena, logger, 
           await motion.distanceHeading(-200, motion.getPose().phi);
           await pause(250);
 
-          if (inSquareC) {
+          if (isPositionInAreaC(halfArenaHeight, motion.getPose())) {
             await motion.move2XY(arenaCenterPosition);
             await pause(250);
           }
@@ -122,6 +147,10 @@ module.exports = (pickupAndReturn = false) => ({ socket, config, arena, logger, 
 
           await motion.distanceHeading(-150, motion.getPose().phi);
           await pause(250);
+
+          await motion.move2XYPhi(verificationPosition, 0);
+          await verifyRotation(lidar, motion, 90, 60);
+          await verifyPosition(arena, lidar, motion, 0);
         } else {
           try {
             await locateCan(config, lidar);
@@ -133,8 +162,7 @@ module.exports = (pickupAndReturn = false) => ({ socket, config, arena, logger, 
           await gripper.setJawAngle(config.GRIPPER_JAW_WIDE_OPEN_ANGLE);
           await gripper.setJawAngle(config.GRIPPER_JAW_OPEN_ANGLE);
 
-          // give "someone" the time to remove the can
-          await pause(5000);
+          await pause(5000); // give "someone" the time to remove the can
         }
 
         matrix[obstacle.row][obstacle.column] = cellStates.EMPTY;
@@ -143,17 +171,12 @@ module.exports = (pickupAndReturn = false) => ({ socket, config, arena, logger, 
 
       const areAllCansStored = numStoredCans === maxNumCans;
 
-      if (localisedCans.length && !areAllCansStored && !isAtLastScanPosition) {
-        await verifyPosition(startPosition);
-      }
-
       if (areAllCansStored || isAtLastScanPosition) {
         const currentPose = motion.getPose();
         const inSquareA = currentPose.x < 500;
-        const currentlyInSquareC = currentPose.y <= halfArenaHeight;
 
         if (!inSquareA) {
-          if (currentlyInSquareC) {
+          if (isPositionInAreaC(halfArenaHeight, currentPose)) {
             console.log('move to center square');
             await motion.move2XY(arenaCenterPosition);
           }
@@ -167,27 +190,8 @@ module.exports = (pickupAndReturn = false) => ({ socket, config, arena, logger, 
     missionComplete();
   }
 
-  async function verifyPosition(startPosition = null) {
-    if (startPosition) {
-      await motion.move2XY(startPosition);
-
-      motion.setTrackPose(false);
-    }
-
-    await solveStartVector(lidar, motion);
-
-    const positionScanData = await scan(lidar, 2000);
-    const positionAveragedMeasurements = averageMeasurements(positionScanData);
-    await gotoStartPosition(positionAveragedMeasurements, motion);
-
-    const currentPositionScanData = await scan(lidar, 2000);
-    const currentPositionAveragedMeasurements = averageMeasurements(currentPositionScanData);
-    const position = getInitialPosition(currentPositionAveragedMeasurements, arena.height);
-
-    motion.setTrackPose(true);
-    motion.appendPose({ ...position, phi: 0 });
-
-    return Promise.resolve(position);
+  function isPositionInAreaC(halfArenaHeight, position) {
+    return position.y < halfArenaHeight;
   }
 
   function stop() {
