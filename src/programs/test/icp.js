c1c823617a96c70a3ddb541421615271973618cb
Erik Noorland <mail@eriknoorland.nl>
2023-10-12 18:07:27 +0200

    Make a simpler icp test

--- src/programs/test/icp.js
+++ src/programs/test/icp.js
@@ -10,34 +10,33 @@ module.exports = () => (logger, config, arena, sensors, actuators, utils, helper
     await startPosition(arena.height);
 
     let heading = 0;
-    console.log('[motion completed mark]', getPose());
 
     await motion.distanceHeading(1000, heading);
-    console.log('[motion completed mark]', getPose());
+    console.log(getPose());
 
     heading = Math.PI / 2;
 
     await motion.rotate(heading);
-    console.log('[motion completed mark]', getPose());
+    console.log(getPose());
 
     await motion.distanceHeading(200, heading);
-    console.log('[motion completed mark]', getPose());
+    console.log(getPose());
 
-    heading = 0;
+    // heading = 0;
 
-    await motion.rotate(heading);
-    console.log('[motion completed mark]', getPose());
+    // await motion.rotate(-(Math.PI / 2));
+    // console.log(getPose());
 
-    await motion.distanceHeading(500, heading);
-    console.log('[motion completed mark]', getPose());
+    // await motion.distanceHeading(500, heading);
+    // console.log(getPose());
 
-    heading = Math.PI;
+    // heading = Math.PI;
 
-    await motion.rotate(heading);
-    console.log('[motion completed mark]', getPose());
+    // await motion.rotate(heading);
+    // console.log(getPose());
 
-    await motion.distanceHeading(1000, heading);
-    console.log('[motion completed mark]', getPose());
+    // await motion.distanceHeading(1000, heading);
+    // console.log(getPose());
 
     eventEmitter.emit('mission_complete');
   }
