67b7d2e6faa9850971c9701fbe014e7d77fac31c
Erik Noorland <mail@eriknoorland.nl>
2023-10-14 08:58:23 +0200

    Can't use async / await with functional array looping, back to the good old days

--- src/hardwareController.js
+++ src/hardwareController.js
@@ -1,20 +1,20 @@
-module.exports = (logger, config, expectedDevices) => {
-  const devices = Object
-    .keys(expectedDevices)
-    .reduce(async (acc, expectedDevice) => {
-      const device = expectedDevices[expectedDevice];
+module.exports = async (logger, config, expectedDevices) => {
+  const expectedDeviceIds = Object.keys(expectedDevices);
+  const devices = {};
 
-      if (device) {
-        try {
-          acc[expectedDevice] = await device.init(device, config);
-          logger.info(`${expectedDevice} initialized!`);
-        } catch (error) {
-          logger.error(error);
-        }
+  for (let i = 0; i < expectedDeviceIds.length; i += 1) {
+    const deviceId = expectedDeviceIds[i];
+    const device = expectedDevices[deviceId];
+
+    if (device) {
+      try {
+        devices[deviceId] = await device.init(device, config);
+        logger.info(`${deviceId} initialized!`);
+      } catch (error) {
+        logger.error(error);
       }
-
-      return acc;
-    }, {});
+    }
+  }
 
   return Promise.resolve(devices);
 };
